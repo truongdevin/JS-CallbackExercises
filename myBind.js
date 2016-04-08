603e5f0a1140f8fb903af6728648854e67936bcd
App Academy Student <student@appacademy.io>
2016-04-08 15:23:03 -0700

    Completed myBind

---
+++ myBind.js
@@ -0,0 +1,27 @@
+Function.prototype.myBind = function (context) {
+  var fn = this;
+
+  return function() {
+    fn.apply(context);
+  };
+
+
+};
+
+function Lamp() {
+   this.name = "a lamp";
+}
+
+var turnOn = function() {
+   console.log("Turning on " + this.name);
+};
+
+var lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+var boundTurnOn = turnOn.bind(lamp);
+var myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
